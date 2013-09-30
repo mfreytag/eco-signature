f4532e66691d2bb70cbb01ebce4d52bb4f7efae4
mfreytag <mfreytag@gmail.com>
2013-09-30 17:20:59 +0200

    modified script

--- signature.sh
+++ signature.sh
@@ -10,6 +10,12 @@ echo "Please enter your 3 digit PHONE EXTENSION:"
 read phoneext
 echo "Please enter your TITLE, e.g. Master of Disaster"
 read title
+echo "Would you like to add your CELL PHONE? (Y/n)"
+read optcell
+if [ $optcell == yes ]
+then echo "Please enter your CELL PHONE number:"
+read cell
+fi
 echo "Generating signature file..."
 
 cat ./signature.htm | sed "s/Toni/$firstname/g" | sed "s/Testa/$lastname/g" | sed "s/86880 666/86880 $phoneext/g" | sed "s/Master of Disaster/$title/g" > output/signature.$firstname.$lastname.htm
