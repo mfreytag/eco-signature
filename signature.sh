8dea87c9fff782a43be3e3367b8f084cf58c2c4e
mfreytag <mfreytag@gmail.com>
2013-09-30 15:58:22 +0200

    simple script

---
+++ signature.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# prompt for fields to be customized
+
+echo "Please enter personal details to be added to your signature file."
+echo "Please enter your LAST NAME:"
+read lastname
+echo "Please enter your FIRST NAME:"
+read firstname
+echo "Please enter your 3 digit PHONE EXTENSION:"
+read phoneext
+echo "Please enter your TITLE, e.g. Master of Disaster"
+read title
+echo "Generating signature file..."
+
+cat ./signature.htm | sed "s/Toni/$firstname/g" | sed "s/Testa/$lastname/g" | sed "s/86880 666/86880 $phoneext/g" | sed "s/Master of Disaster/$title/g" > output/signature.$firstname.$lastname.htm
